f6d979da430825b3d3801b4169aa32109302fae9
essammetwally1 <essammetwally1@gmail.com>
2025-09-24 19:42:01 +0300

    initial commit

---
+++ lib/app_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primary = Color(0xFF0D1B2A);
+  static const Color red = Color(0xFFB22222);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color green = Color(0xFF228B22);
+
+  static ThemeData lightTheme = ThemeData();
+  static ThemeData darkTheme = ThemeData();
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:snapalyze/app_theme.dart';
+import 'package:snapalyze/screens/home_screen.dart';
+
+void main() {
+  runApp(Snapalyze());
+}
+
+class Snapalyze extends StatelessWidget {
+  const Snapalyze({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {HomeScreen.routeName: (context) => HomeScreen()},
+      initialRoute: HomeScreen.routeName,
+
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.light,
+    );
+  }
+}
